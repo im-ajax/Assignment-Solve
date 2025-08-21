74ce2df739fdabe37188baf785f4cebe78f37804
im-ajax <saniahmed0480@gmail.com>
2025-08-21 22:22:27 +0600

    added spl file

---
+++ SPL-Mid-Term-solves/spl_mid_solve.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<stdlib.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            printf("* ");
+        }
+    }
+    
+    
+    return 0;
+}

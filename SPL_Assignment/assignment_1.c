0eab3efefba1c06abab6705cfb0e31ddfd941f27
im-ajax <saniahmed0480@gmail.com>
2025-08-29 18:37:57 +0600

    added the new assignment

---
+++ SPL_Assignment/assignment_1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<stdlib.h>
+int main(){
+    int size;
+    scanf("%d",&size);
+    int arr[size];
+    for (int i = 0; i < size; i++){
+        scanf("%d",&arr[i]);
+    }
+    for (int i = size-1; i <= 0; i++)
+    {
+        printf()
+    }
+    
+    return 0;
+}

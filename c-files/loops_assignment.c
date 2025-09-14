825824bbe3ac71b0d630c9bacdfcad12dac82840
im-ajax <saniahmed0480@gmail.com>
2025-09-14 11:18:56 +0600

    adding files

--- c-files/loops_assignment.c
+++ c-files/loops_assignment.c
@@ -1,11 +1,9 @@
-//loops assignments here.
-// 3 5 6 9 10 12 13 14 15 16 18 19 20
+// loops assignments here.
+//  3 5 6 9 10 12 13 14 15 16 18 19 20
 
-
-//question 1
-//Write a program (WAP) that will print following series upto Nth terms.
-//1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, …….
-
+// question 1
+// Write a program (WAP) that will print following series upto Nth terms.
+// 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, …….
 
 // #include<stdio.h>
 // #include<math.h>
@@ -16,19 +14,12 @@
 //     scanf("%d",&n);
 //     for (int i = 1; i <= n; i++)
 //         printf("%d ",i);
-//     return 0;  
+//     return 0;
 // }
 
-
-//question 2
-// Write a program (WAP) that will print following series upto Nth terms.
-//1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31 …….
-
-
-
-
-
-
+// question 2
+//  Write a program (WAP) that will print following series upto Nth terms.
+// 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31 …….
 
 // #include<stdio.h>
 // #include<math.h>
@@ -41,13 +32,13 @@
 //         printf("%d,",num);
 //         num+=2;
 //     }
-    
-//     return 0;  
+
+//     return 0;
 // }
 
-//question 3
-//Write a program (WAP) that will print following series upto Nth terms.
-//1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, …….
+// question 3
+// Write a program (WAP) that will print following series upto Nth terms.
+// 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, …….
 
 // #include<stdio.h>
 // #include<math.h>
@@ -65,9 +56,8 @@
 //     return 0;
 // }
 
-
-//question 4
-//Write a program (WAP) that will take N numbers as inputs and compute their average. 
+// question 4
+// Write a program (WAP) that will take N numbers as inputs and compute their average.
 //(Restriction: Without using any array)
 
 // #include<stdio.h>
@@ -88,8 +78,8 @@
 //     return 0;
 // }
 
-//question 5
-//Write a program (WAP) that will take two numbers X and Y as inputs. Then it will print the square of X and increment (if X<Y) or decrement (if X>Y) X by 1, until X reaches Y. If and when X is equal to Y, the program prints “Reached!”
+// question 5
+// Write a program (WAP) that will take two numbers X and Y as inputs. Then it will print the square of X and increment (if X<Y) or decrement (if X>Y) X by 1, until X reaches Y. If and when X is equal to Y, the program prints “Reached!”
 
 // #include<stdio.h>
 // #include<math.h>
@@ -114,14 +104,14 @@
 //             if(i==y) printf("Reached"); //brackets deleted to avoid complexity while understanding the code.
 //             else printf("%d ",i*i);
 //         }
-    
+
 //     }
 //     return 0;
 // }
 
-//question 6
+// question 6
 /*
-    Write a program (WAP) for the described scenario: 
+    Write a program (WAP) for the described scenario:
     Player-1 picks a number X and Player-2 has to guess that number within N tries. For each wrong guess by Player-2, the program prints “Wrong, N-1 Choice(s) Left!” If Player-2 at any time successfully guesses the number, the program prints “Right, Player-2 wins!” and terminates right away. Otherwise after the completion of N wrong tries, the program prints “Player-1 wins!” and halts.
     (Hint: Use break/continue)
 */
@@ -149,8 +139,8 @@
 //     return 0;
 // }
 
-//question 7
-//Write a program (WAP) that will run and show keyboard inputs until the user types an ’A’ at the keyboard. 
+// question 7
+// Write a program (WAP) that will run and show keyboard inputs until the user types an ’A’ at the keyboard.
 
 // #include<stdio.h>
 // #include<math.h>
@@ -170,9 +160,8 @@
 //     return 0;
 // }
 
-
-//question 8
-//Write a program (WAP) that will reverse the digits of an input integer.
+// question 8
+// Write a program (WAP) that will reverse the digits of an input integer.
 
 // #include<stdio.h>
 // #include<math.h>
@@ -191,30 +180,74 @@
 //     return 0;
 // }
 
+// question 9
+// Write a program (WAP) that will find the grade of N students. For each student, it will take the marks of his/her the attendance (on 5 marks), assignment (on 10 marks), class test (on 15 marks), midterm (on 50 marks), term final (on 100 marks). Then based on the tables shown below, the program will output his grade.
 
-//question 9
-//Write a program (WAP) that will find the grade of N students. For each student, it will take the marks of his/her the attendance (on 5 marks), assignment (on 10 marks), class test (on 15 marks), midterm (on 50 marks), term final (on 100 marks). Then based on the tables shown below, the program will output his grade.
-
-// #include<stdio.h>
-// #include<math.h>
-// #include<string.h>
-// #include<stdlib.h>
-// int main(){
-    
+// #include <stdio.h>
+// int main() {
+//     int marks;
+//     printf("Enter marks: ");
+//     scanf("%d", &marks);
+//     if (marks > 100 || marks < 0) {
+//         printf("Invalid input\n");
+//     } else {
+//         do {
+//             if (marks >= 90) {
+//                 printf("Grade: A\n");
+//                 break;
+//             }
+//             if (marks >= 86) {
+//                 printf("Grade: A-\n");
+//                 break;
+//             }
+//             if (marks >= 82) {
+//                 printf("Grade: B+\n");
+//                 break;
+//             }
+//             if (marks >= 78) {
+//                 printf("Grade: B\n");
+//                 break;
+//             }
+//             if (marks >= 74) {
+//                 printf("Grade: B-\n");
+//                 break;
+//             }
+//             if (marks >= 70) {
+//                 printf("Grade: C+\n");
+//                 break;
+//             }
+//             if (marks >= 66) {
+//                 printf("Grade: C\n");
+//                 break;
+//             }
+//             if (marks >= 62) {
+//                 printf("Grade: C-\n");
+//                 break;
+//             }
+//             if (marks >= 58) {
+//                 printf("Grade: D+\n");
+//                 break;
+//             }
+//             if (marks >= 55) {
+//                 printf("Grade: D\n");
+//                 break;
+//             }
+//             printf("Grade: F\n");
+//         } while (0);
+//     }
 //     return 0;
 // }
 
-
-//question 10
-//Write a program (WAP) that will give the sum of first Nth terms for the following series.
-//1, -2, 3, -4, 5, -6, 7, -8, 9, -10, 11, -12, 13, -14, …….
+// question 10
+// Write a program (WAP) that will give the sum of first Nth terms for the following series.
+// 1, -2, 3, -4, 5, -6, 7, -8, 9, -10, 11, -12, 13, -14, …….
 
 // #include<stdio.h>
 // #include<math.h>
 // #include<string.h>
 // #include<stdlib.h>
 // int main(){
-//     int n,sum=0; 
+//     int n,sum=0;
 //     scanf("%d",&n);
 //     for (int i = 1; i <= n; i++){
 //         if(i%2==0)
@@ -225,9 +258,9 @@
 //     return 0;
 // }
 
-//question 11
-//Write a program (WAP) that will calculate the result for the first Nth terms of the following series.  [In that series sum, dot sign (.) means multiplication]
-// 1^2.2 + 2^2.3 + 3^2.4 + 4^2.5 + …….
+// question 11
+// Write a program (WAP) that will calculate the result for the first Nth terms of the following series.  [In that series sum, dot sign (.) means multiplication]
+//  1^2.2 + 2^2.3 + 3^2.4 + 4^2.5 + …….
 
 // #include<stdio.h>
 // #include<math.h>
@@ -244,9 +277,9 @@
 //     return 0;
 // }
 
-//qeusiton 12
-//Write a program (WAP) that will print Fibonacci series upto Nth terms.
-//1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, …….
+// qeusiton 12
+// Write a program (WAP) that will print Fibonacci series upto Nth terms.
+// 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, …….
 
 // #include<stdio.h>
 // #include<math.h>
@@ -265,9 +298,8 @@
 //     return 0;
 // }
 
-
-//question 13
-//Write a program (WAP) that will print the factorial (N!) of a given number N. Please see the sample input output.
+// question 13
+// Write a program (WAP) that will print the factorial (N!) of a given number N. Please see the sample input output.
 
 // #include<stdio.h>
 // #include<math.h>
@@ -282,75 +314,69 @@
 //     return 0;
 // }
 
-
-//question 14
-// Write a program (WAP) that will find nCr where n >= r; n and r are integers.
-//#include<stdio.h>
-// #include<math.h>
-// #include<string.h>
-// #include<stdlib.h>
-// int main(){
-//     int n,r;
-//     scanf("%d%d",&n,&r);
-//     int res=1; // n! er value ber kortesi
-//     for (int i = n; i >= 1; i--){
-//         res=res*i;
-//     }
-//     long long res2=1; // r! er value ber kortesi
-//     for (int i = r; i >= 1; i--){
-//         res2*=i;
-//     }
-//     // (n-r)! er value ber kortesi
-//     int third_factorial= (n-r);
-//     long long res3=1;
-//     for (int i = third_factorial; i >=1 ; i--){
-//         res3*=i;
-//     }
-//     long long total= res / (res2 * res3);
-//     printf("total : %lld",total);
-//     return 0;
-// }
-
-//question 15
-//Write a program (WAP) that will find xy (x to the power y) where x, y are positive integers.
+// question 14
+//  Write a program (WAP) that will find nCr where n >= r; n and r are integers.
 // #include<stdio.h>
-// #include<math.h>
-// #include<string.h>
-// #include<stdlib.h>
-// int main(){
-//     int num,pow,result;
-//     scanf("%d%d",&num,&pow);
-//     for (int i = 0; i < pow; i++)
-//         result=num*num;
-//     printf("%d",result);
-//     return 0;
-// }
+//  #include<math.h>
+//  #include<string.h>
+//  #include<stdlib.h>
+//  int main(){
+//      int n,r;
+//      scanf("%d%d",&n,&r);
+//      int res=1; // n! er value ber kortesi
+//      for (int i = n; i >= 1; i--){
+//          res=res*i;
+//      }
+//      long long res2=1; // r! er value ber kortesi
+//      for (int i = r; i >= 1; i--){
+//          res2*=i;
+//      }
+//      // (n-r)! er value ber kortesi
+//      int third_factorial= (n-r);
+//      long long res3=1;
+//      for (int i = third_factorial; i >=1 ; i--){
+//          res3*=i;
+//      }
+//      long long total= res / (res2 * res3);
+//      printf("total : %lld",total);
+//      return 0;
+//  }
 
+// question 15
+// Write a program (WAP) that will find xy (x to the power y) where x, y are positive integers.
+//  #include<stdio.h>
+//  #include<math.h>
+//  #include<string.h>
+//  #include<stdlib.h>
+//  int main(){
+//      int num,pow,result;
+//      scanf("%d%d",&num,&pow);
+//      for (int i = 0; i < pow; i++)
+//          result=num*num;
+//      printf("%d",result);
+//      return 0;
+//  }
 
+// question 16
+// WAP that will find the GCD (greatest common divisor) and LCM (least common multiple) of two positive integers.
+//  #include<stdio.h>
+//  #include<math.h>
+//  #include<string.h>
+//  #include<stdlib.h>
+//  int main(){
+//      int num1,num2,temp;
+//      scanf("%d%d",&num1,&num2);
+//      while(num2!=0){
+//          temp=num1%num2;
+//          num1=num2;
+//          num2=temp;
+//      }
+//      printf("%d",num1);
+//      return 0;
+//  }
 
-
-
-//question 16
-//WAP that will find the GCD (greatest common divisor) and LCM (least common multiple) of two positive integers.
-// #include<stdio.h>
-// #include<math.h>
-// #include<string.h>
-// #include<stdlib.h>
-// int main(){
-//     int num1,num2,temp;
-//     scanf("%d%d",&num1,&num2);
-//     while(num2!=0){
-//         temp=num1%num2;
-//         num1=num2;
-//         num2=temp;
-//     }
-//     printf("%d",num1);
-//     return 0;
-// }
-
-
-//question 17
-//WAP that will determine whether a number is prime or not.
+// question 17
+// WAP that will determine whether a number is prime or not.
 
 // #include<stdio.h>
 // #include<math.h>
@@ -373,33 +399,46 @@
 //     return 0;
 // }
 
-//question 18
-//WAP that will determine whether an integer is palindrome number or not.
-// #include<stdio.h>
-// #include<math.h>
-// #include<string.h>
-// #include<stdlib.h>
-// int main(){
-//     int num,digits,rev_dig;
-//     scanf("%d",&num);
-//     for (int i = 0; i < num; i++)
-//     {
-//         rev_dig=num%10;
-//         num=num/10;
-//     }
-    
+// question 18
+// WAP that will determine whether an integer is palindrome number or not.
+//  #include<stdio.h>
+//  #include<math.h>
+//  #include<string.h>
+//  #include<stdlib.h>
+//  int main(){
+//      int num,digits,rev_dig;
+//      scanf("%d",&num);
+//      for (int i = 0; i < num; i++)
+//      {
+//          rev_dig=num%10;
+//          num=num/10;
+//      }
+
 //     return 0;
 // }
 
-//question 10
-//WAP that will calculate following mathematical function for the input of x. Use only the series to solve the problem.
-// sinx = x-x^3/3! + x^5/5! + x^7/7! + ....... INFINITY
+// question 19
+// WAP that will calculate following mathematical function for the input of x. Use only the series to solve the problem.
+//  sinx = x-x^3/3! + x^5/5! + x^7/7! + ....... INFINITY
+#include <stdio.h>
+int main()
+{
+    double x, term, sum;
+    int i;
+    scanf("%lf", &x);
+    term = x;
+    sum = x;
+    for (i = 1; i <= 15; i++){
+        term = term * (-1.0) * x * x / ((2 * i) * (2 * i + 1));
+        sum = sum + term;
+    }
+    printf("%.3f\n", sum);
+    return 0;
+}
 
-
-
-//question 20
-// Write a program that takes an integer number n as input and find out the sum of the following series up to n terms.
-// 1 + 12 + 123 + 1234 + …….
+// question 20
+//  Write a program that takes an integer number n as input and find out the sum of the following series up to n terms.
+//  1 + 12 + 123 + 1234 + …….
 
 // #include<stdio.h>
 // #include<math.h>
@@ -424,9 +463,3 @@
 //     flag==1? printf("Found\n") : printf("Not Found\n");
 //     return 0;
 //}
-
-
-
-
-
-
